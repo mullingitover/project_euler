0cf8ac9b796f0e55e9dffb04228da653994ea89a
Barry Mulling <Barry.Mulling@aofl.com>
2018-09-06 18:27:28 -0700

    update problem 3 in python to use class

--- python/003/problem_3.py
+++ python/003/problem_3.py
@@ -5,20 +5,21 @@ What is the largest prime factor of the number 600851475143 ?
 '''
 import math
 
-def primefactors(n):
+class PrimeFactorFinder:
+    def primefactors(self,n):
 
-    # Get rid of the twos
-    while n % 2 == 0:
-        print 2,
-        n = n / 2
-    
-    # Now that n is odd and we've factored out twos, we can skip them
-    for i in range(3,int(math.sqrt(n))+1,2):
-        while n % i == 0:
-            print i,
-            n = n / i
+        # Get rid of the twos
+        while n % 2 == 0:
+            print 2,
+            n = n / 2
+        
+        # Now that n is odd and we've factored out twos, we can skip them
+        for i in range(3,int(math.sqrt(n))+1,2):
+            while n % i == 0:
+                print i,
+                n = n / i
 
-    if n > 2: 
-        print n 
+        if n > 2: 
+            print n 
 
-print primefactors(600851475143)
+print(PrimeFactorFinder().primefactors(600851475143))
